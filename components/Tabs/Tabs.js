f57b0f9bc3e8480398816c354e5f7c1317ffce91
kmcknight1 <katiemcknight1991@gmail.com>
2019-05-17 12:33:41 -0500

    added carousel and slide animation

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,10 +5,8 @@ class TabLink {
     this.tabData = this.tabElement.dataset.tab;
 
     if (this.tabData === "all") {
-      // If `all` is true, select all cards regardless of their data attribute values
       this.cards = document.querySelectorAll('.card');
     } else {
-      // else if `all` is false, only select the cards with matching this.tabData values
       this.cards = document.querySelectorAll(`.card[data-tab="${this.tabData}"]`);
     }
     
@@ -19,32 +17,25 @@ class TabLink {
     console.log("TabLink", "Element", this.tabElement, "data", this.tabData, "cards", this.cards);
 
 
-    // Add a click event that invokes this.selectTab
     this.tabElement.addEventListener('click', () => this.selectTab());
   }
 
   selectTab(){
 
-    // Select all elements with the .tab class on them
     const tabs = document.querySelectorAll('.tab');
     
-    // Iterate through the NodeList removing the .active-tab class from each element
     tabs.forEach(tab => {
       tab.classList.remove('active-tab');
     })
 
-    // Select all of the elements with the .card class on them
     const cards = document.querySelectorAll('.card');
 
-    // Iterate through the NodeList setting the display style each one to 'none'
     cards.forEach(card => {
       card.style.display = "none";
     })
     
-    // Add a class of ".active-tab" to this.tabElement
     this.tabElement.classList.add('active-tab');
   
-    // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
     this.cards.forEach(card => card.selectCard());
   }
 }
@@ -56,21 +47,12 @@ class TabCard {
     console.log("TabCard", this.cardElement);
   }
   selectCard(){
-    // Update the style of this.cardElement to display = "flex"
     this.cardElement.style.display = "flex";
   }
 
 }
 
-/* START HERE: 
 
-- Select all classes named ".tab" and assign that value to the tabs variable
-
-- With your selection in place, now chain a .forEach() method onto the tabs variable to iterate over the DOM NodeList
-
-- In your .forEach() method's callback function, return a new instance of TabLink and pass in each tab as a parameter
-
-*/
 let tabs = document.querySelectorAll('.tab');
 
 tabs.forEach(tab => {
